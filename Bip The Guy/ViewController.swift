11979d3be6db752d6ba7e2e49f064f6d56f2d365
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-02-06 13:57:30 -0500

    UI Setup

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func libraryPressed(_ sender: UIButton) {
+    }
 
+    @IBAction func cameraPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        print("Hey!")
+    }
 }
 
