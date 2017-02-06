61b5a788bf52bbc87f1902a85e7a13104f846a06
Dom Ronan <dominickronan@doms-mbp.bc.edu>
2017-02-06 14:42:48 -0500

    sound and animations

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
     @IBOutlet weak var imageToPunch: UIImageView!
     
-    
+    var audioPlayer = AVAudioPlayer()
+    var imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +27,32 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // MARK: Functions
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        
+        //shrink our image to punch by 60 pixels
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + shrinkValue, y: self.imageToPunch.bounds.origin.y + shrinkValue, width: self.imageToPunch.bounds.size.width - shrinkValue, height: self.imageToPunch.bounds.size.height - shrinkValue)
+        
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: { self.imageToPunch.bounds = bounds }, completion: nil)
+    }
+    
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer){
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: Data from\(soundName) could not be played as an audio file")
+            }
+        } else {
+            print("ERROR: Could not load data from file \(soundName)")
+        }
+    }
+    
+    // MARK: Actions
+    
     @IBAction func libraryPressed(_ sender: UIButton) {
     }
 
@@ -32,7 +61,8 @@ class ViewController: UIViewController {
     
     
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        print("Hey!")
+       animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
 }
 
